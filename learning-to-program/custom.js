8ed7fe0c716d2b96a5af5d9342b991e0a85410e1
rommel santiago <rommelsantiago@rommels-Mac-Pro.local>
2017-11-08 12:39:47 -0800

    js renamed, kittenbook.html, added clock

--- learning-to-program/custom.js
+++
@@ -1,4 +0,0 @@
-// Chapter 1: Hello World, Your First Program!
-// textbook
-var userName = prompt('What\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>';
---
+++ learning-to-program/js/prompt.js
@@ -0,0 +1,6 @@
+// Chapter 1: Hello World, Your First Program!
+// textbook
+var userName = prompt('What\'s your name?');
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
+                          '<p>' + projectName + ' ' + versionNumber +
+                          ' accessed on: ' + currentTime; + '</p>';
